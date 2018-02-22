7ae008da8ca8e1d5be23858b0b01ca208262df5e
Frank Thelen <frank.thelen@telefonica.com>
2018-02-22 19:14:12 +0100

    initial commit

---
+++ src/chai-quantifiers.js
@@ -0,0 +1,40 @@
+const evaluate = (array, predicate) =>
+  array.reduce(
+    (acc, item, i) =>
+      (predicate(item) ? ({ t: [...acc.t, i], f: acc.f }) : ({ t: acc.t, f: [...acc.f, i] })),
+    { t: [], f: [] }
+  );
+
+const plugin = (chai) => {
+  chai.Assertion.addMethod('containAll', function func(predicate) {
+    const array = this._obj; // eslint-disable-line no-underscore-dangle
+    const { f } = evaluate(array, predicate);
+    this.assert(
+      f.length === 0,
+      `expected array to contain all items to be true, but items at indexes [${f}] are false`,
+      'expected array not to contain all items to be true, but all items are true'
+    );
+  });
+
+  chai.Assertion.addMethod('containOne', function func(predicate) {
+    const array = this._obj; // eslint-disable-line no-underscore-dangle
+    const { t } = evaluate(array, predicate);
+    this.assert(
+      t.length >= 1,
+      'expected array to contain one or more items to be true, but all items are false',
+      `expected array not to contain one or more items to be true, but items at indexes [${t}] are true`
+    );
+  });
+
+  chai.Assertion.addMethod('containExactlyOne', function func(predicate) {
+    const array = this._obj; // eslint-disable-line no-underscore-dangle
+    const { t } = evaluate(array, predicate);
+    this.assert(
+      t.length === 1,
+      `expected array to exactly contain one item to be true, but items at indexes [${t}] are true`,
+      `expected array not to exactly contain one item to be true, but item at index [${t}] is true`
+    );
+  });
+};
+
+module.exports = plugin;
---
+++ test/chai-quantifiers.spec.js
@@ -0,0 +1,48 @@
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const chaiQuantifiers = require('..');
+
+chai.use(chaiAsPromised);
+chai.use(chaiQuantifiers);
+
+const { expect } = chai;
+
+describe('chai-quentifiers', () => {
+  describe('containAll', () => {
+    it('should be true if all items are true', () => {
+      expect([0, 1, 2, 3]).to.containAll(item => item < 4);
+    });
+
+    it('should be false if one item is false', () => {
+      expect([0, 1, 2, 3]).to.not.containAll(item => item >= 3);
+    });
+  });
+
+  describe('containOne', () => {
+    it('should be true if one item is true', () => {
+      expect([0, 1, 2, 3]).to.containOne(item => item === 2);
+    });
+
+    it('should be true if two items are true', () => {
+      expect([0, 1, 2, 3]).to.containOne(item => item >= 2);
+    });
+
+    it('should be false if no item is true', () => {
+      expect([0, 1, 2, 3]).to.not.containOne(item => item > 3);
+    });
+  });
+
+  describe('containExactlyOne', () => {
+    it('should be true if one item is true', () => {
+      expect([0, 1, 2, 3]).to.containExactlyOne(item => item === 2);
+    });
+
+    it('should be false if two items are true', () => {
+      expect([0, 1, 2, 3]).to.not.containExactlyOne(item => item >= 2);
+    });
+
+    it('should be false if no item is true', () => {
+      expect([0, 1, 2, 3]).to.not.containExactlyOne(item => item > 3);
+    });
+  });
+});
